b92b6ed7963a76f4c8ec218de3f0e0ec49c084c8
A-BH75 <amarzooq10@gmail.com>
2024-11-25 16:44:20 +0300

    Update client.py
    option 2 and Invalid option

--- client.py
+++ client.py
@@ -17,9 +17,9 @@ while True:
         print("1.Search headlines ")
         print("2.List of sources")
         print("3.Quit")
-       option = input(int("your option is : "))
+        option = input(int("your option is : "))
 
-       if option == "1":
+        if option == "1":
             print("Choose the number of your option")
             print("1.search for key word")
             print("2.search for caregory")
@@ -27,10 +27,24 @@ while True:
             print("3.list all new headlines")
             print("4.back to the main menu")
             option2 = input(int("your option is : "))
-    
 
-    
+        elif option == "2":
+            print("Choose the number of your option")
+            print("1.search for caregory")
+            print("2.search for country")
+            print("3.search by language")
+            print("4.list all")
+            print("5.back to the main menu")
+            option3 = input(int("your option is : "))
+        
+        else:
+            print("Invalid option")
+
+
+            
+
     
     
     except :    
         break
+
