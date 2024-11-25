db52c8a600bb969bfa6dba7fcec26d780fb96b5a
A-BH75 <amarzooq10@gmail.com>
2024-11-25 15:57:07 +0300

    Update client.py
    adding socket

--- client.py
+++ client.py
@@ -1,1 +1,9 @@
-#client side 
+import socket
+
+client_Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    client_Sock.connect(('192.168.0.1',5000))
+except:
+    print("connaction  faild")
+    exit(0) 
