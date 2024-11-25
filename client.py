17786eaa8cd41a65f3beb78feb46c6fe5ff1a24b
A-BH75 <amarzooq10@gmail.com>
2024-11-25 16:36:00 +0300

    Update client.py
    option 1

--- client.py
+++ client.py
@@ -17,7 +17,19 @@ while True:
         print("1.Search headlines ")
         print("2.List of sources")
         print("3.Quit")
-        option = input(int("your option is : "))
+       option = input(int("your option is : "))
+
+       if option == "1":
+            print("Choose the number of your option")
+            print("1.search for key word")
+            print("search for caregory")
+            print("search for country")
+            print("list all new headlines")
+            print("back to the main menu")
+            option2 = input(int("your option is : "))
+    
+
+    
     
     
     except :    
